e60d7d60468084cf7aa2a2e44b424ef548c21c9d
Pierniki <jjdroid6969@gmail.com>
2022-10-06 14:54:58 +0200

    fix: fix codegen generating duplicate types

--- codegen.ts
+++ codegen.ts
@@ -3,18 +3,16 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 const config: CodegenConfig = {
   overwrite: true,
   schema: 'https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/cl8suah0k41u501t8870qd24t/master',
-  documents: 'src/**/*.ts',
+  documents: 'src/lib/*.ts',
   verbose: true,
   generates: {
     'src/gql': {
       config: {
-        flattenGeneratedTypes: true,
-        flattenGeneratedTypesIncludeFragments: true,
-        exportFragmentSpreadSubTypes: true,
-        mergeFragmentTypes: true
+        operationResultSuffix: 'Result',
+        onlyOperationTypes: true
       },
       preset: 'client',
-      plugins: ['typescript', 'typescript-operations', 'typescript-graphql-request']
+      plugins: []
     }
   }
 };
